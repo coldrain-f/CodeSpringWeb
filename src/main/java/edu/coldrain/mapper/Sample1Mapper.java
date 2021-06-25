d0f45b76ee96740e7d6d43dad0d3758a99496935
coldrain-f <intmain__@naver.com>
2021-06-25 11:18:00 +0900

    479p 트랜잭션 실습 완료

---
+++ src/main/java/edu/coldrain/mapper/Sample1Mapper.java
@@ -0,0 +1,9 @@
+package edu.coldrain.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+
+public interface Sample1Mapper {
+
+	@Insert("INSERT INTO TBL_SAMPLE1 (COL1) VALUES (#{data})")
+	public int insertCol1(String data);
+}
